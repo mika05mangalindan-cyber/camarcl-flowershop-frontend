8aa1eeecea93a85ae8ce6be9618a1928e5b362e7
Miks0503 <mika05delenciamangalindan@gmail.com>
2025-11-27 00:30:57 +0800

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,7 +43,7 @@ export default function Login({ onLogin }) {
         <img
           src="/plant.jpg"
           alt="Login Illustration"
-          className="w-full h-full object-cover"
+          className="absolute inset-0 w-full h-full object-cover"
         />
       </div>
 
