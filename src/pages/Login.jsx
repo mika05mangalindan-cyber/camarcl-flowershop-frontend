790599f007f02121659c802a82e509e0fd8b9621
Miks0503 <mika05delenciamangalindan@gmail.com>
2025-11-26 22:24:25 +0800

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -85,13 +85,14 @@ export default function Login({ onLogin }) {
                 placeholder="********"
                 required
               />
-              <button
+             <button
                 type="button"
-                className="absolute right-3 top-9 text-gray-500 hover:text-gray-700"
+                className="absolute right-3 top-9 text-gray-500 hover:text-gray-700 cursor-pointer"
                 onClick={() => setShowPassword(!showPassword)}
-              >
+                >
                 {showPassword ? "Hide" : "Show"}
-              </button>
+                </button>
+
             </div>
 
             <button
