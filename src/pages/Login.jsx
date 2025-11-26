0a136245bc574a5008e07ed3a378637d54c5372b
Miks0503 <mika05delenciamangalindan@gmail.com>
2025-11-26 22:12:40 +0800

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,7 +41,7 @@ export default function Login({ onLogin }) {
       {/* Left side image */}
       <div className="hidden md:flex md:w-1/2 bg-green-700 items-center justify-center">
         <img
-          src="../public/logo.png" // put logo in public folder
+          src="./public/logo.png"
           alt="Login Illustration"
           className="max-w-xs md:max-w-md mx-auto"
         />
